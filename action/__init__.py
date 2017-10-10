4b6f45bbb7a5a4592febb9388162432716d90012
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-10 13:10:04 +0200

    Base for front-end.

--- action/__init__.py
+++ action/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 
 from action.models import db, migrate
+from action.extensions import mail
 from action.api import api
+from action.frontend import frontend
 
 
 def create_app(config_file='config/default.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
     app.register_blueprint(api)
+    app.register_blueprint(frontend)
     db.init_app(app)
+    mail.init_app(app)
     migrate.init_app(app, db)
 
     return app
