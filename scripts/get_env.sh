223eaf38868b6a4e012ce3e8ae9461a2e040ba0e
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-10 18:51:28 +0200

    Please God.

--- scripts/get_env.sh
+++ scripts/get_env.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 set -a
-source ./.saved_env
+source ./.env
 set +a
--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -5,6 +5,3 @@ DATABASE_URL="$DATABASE_URL"
 FLASK_APP="$FLASK_APP"
 SECRET_KEY="$SECRET_KEY"
 EOF
-
-echo "Saved env vars in $1"
-ls -a
