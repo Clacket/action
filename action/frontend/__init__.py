72edf3c0a1bda26b5a4c97356b8eb7c63ec71cde
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-12 02:27:20 +0200

    Certbot challenge.

--- action/frontend/__init__.py
+++ action/frontend/__init__.py
@@ -8,3 +8,10 @@ frontend = Blueprint(
 @frontend.route('/')
 def index():
     return render_template('index.html')
+
+
+@frontend.route('/.well-known/acme-challenge/'
+                'rK9944ZpatukKmgBTg_qWamQ2zP23NaWHFDygo8_r5g')
+def challenge():
+    return 'rK9944ZpatukKmgBTg_qWamQ2zP23NaWHFDygo8_r5g.'\
+            '1dYUIFasTS8DhTLxaz2kpn47w20_TLA32KNVvEWSbLY'
