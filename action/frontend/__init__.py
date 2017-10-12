b5ed02f4ba3ef6826ad0a2f1d3e5463f8a2a3946
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-12 02:44:28 +0200

    Remove challenge.

--- action/frontend/__init__.py
+++ action/frontend/__init__.py
@@ -8,10 +8,3 @@ frontend = Blueprint(
 @frontend.route('/')
 def index():
     return render_template('index.html')
-
-
-@frontend.route('/.well-known/acme-challenge/'
-                'rK9944ZpatukKmgBTg_qWamQ2zP23NaWHFDygo8_r5g')
-def challenge():
-    return 'rK9944ZpatukKmgBTg_qWamQ2zP23NaWHFDygo8_r5g.'\
-            '1dYUIFasTS8DhTLxaz2kpn47w20_TLA32KNVvEWSbLY'
