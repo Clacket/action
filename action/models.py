62dceb3c3b3c1870048d21a14cd9d0ebe120dd0b
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-12 16:29:39 +0200

    Update database model for prototype.

--- action/models.py
+++ action/models.py
@@ -8,10 +8,10 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
-class Cinema(db.Model):
-    """The Cell table.
+class Showing(db.Model):
+    """The Cinema/Channel table.
     """
-    __tablename__ = 'cinema'
+    __tablename__ = 'showing'
 
     id = db.Column(db.BigInteger, autoincrement=True, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -21,6 +21,35 @@ class Cinema(db.Model):
     phone = db.Column(db.String)
     geometry = db.Column(ga.Geography('POLYGON', srid=4326,
                                       spatial_index=False))
+    movies = db.relationship(
+        'Movie', secondary='movie_showing',
+        back_populates='showings', lazy='dynamic')
+
+
+class Movie(db.Model):
+    """The Movie table."""
+
+    __tablename__ = 'movie'
+
+    id = db.Column(db.BigInteger, autoincrement=True, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    showings = db.relationship(
+        'Showing', secondary='movie_showing',
+        back_populates='movies', lazy='dynamic')
+
+
+class MovieShowing(db.Model):
+    """Relationship between movie and showing."""
+
+    __tablename__ = 'movie_showing'
+
+    movie_id = db.Column(
+        db.BigInteger, db.ForeignKey('movie.id'), primary_key=True)
+    showing_id = db.Column(
+        db.BigInteger, db.ForeignKey('showing.id'), primary_key=True)
+    time_from = db.Column(db.DateTime)
+    time_to = db.Column(db.DateTime)
 
 
 # Utility functions.
@@ -37,6 +66,6 @@ def create_geo_index(column):
 
 # Create geometry indexes:
 geo_indexes = []
-for table_class in [Cinema]:
+for table_class in [Showing]:
     column = table_class.__table__.c['geometry']
     geo_indexes.append(create_geo_index(column))
