c1a3a2f2d26e648e5f933bd073e27ba9474a618e
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-13 16:45:08 +0200

    Remove challenges.

--- action/admin/__init__.py
+++ action/admin/__init__.py
@@ -6,10 +6,3 @@ admin = Blueprint('admin', __name__, subdomain='admin')
 @admin.route('/')
 def index():
     return 'Hi, Admin'
-
-
-@admin.route('/.well-known/acme-challenge/'
-             '_nL7FhKCWZDsOVKjOLBu3aRaxYm-ceYhSzNN9f1PBNY')
-def challenge():
-    return '_nL7FhKCWZDsOVKjOLBu3aRaxYm-ceYhSzNN9f1PBNY.'\
-           '1dYUIFasTS8DhTLxaz2kpn47w20_TLA32KNVvEWSbLY'
