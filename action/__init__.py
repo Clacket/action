f5fd3c7595dbc21f1e07f9cc03b170da3b5c4fce
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-17 00:46:24 +0200

    Force HTTPS

--- action/__init__.py
+++ action/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, _request_ctx_stack
 
 from action.models import db, migrate
-from action.extensions import mail
+# from action.extensions import mail
 from action.api import api
 from action.frontend import frontend
 from action.admin import admin
@@ -14,10 +14,20 @@ def create_app(config_file='config/default.py'):
     app.register_blueprint(frontend)
     app.register_blueprint(admin)
     db.init_app(app)
-    mail.init_app(app)
+    # mail.init_app(app)
     migrate.init_app(app, db)
 
     return app
 
 
 app = create_app()
+
+
+def force_https():
+    if not app.debug:
+        if _request_ctx_stack is not None:
+            reqctx = _request_ctx_stack.top
+            reqctx.url_adapter.url_scheme = 'https'
+
+
+app.before_request(force_https)
