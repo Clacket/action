df370675f087c336ceb65427b61620287ad47f6b
Mariam Maarouf <mrf.mariam@gmail.com>
2017-10-21 18:56:02 +0200

    Hello Clacket frontend.

--- action/frontend/__init__.py
+++ action/frontend/__init__.py
@@ -2,9 +2,15 @@ from flask import Blueprint, render_template
 
 frontend = Blueprint(
     'frontend', __name__, static_folder='static',
+    static_url_path='/frontend/static',
     template_folder='templates')
 
 
 @frontend.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('frontend_index.html')
+
+
+@frontend.route('/cinemas')
+def cinemas():
+    return render_template('frontend_cinemas.html')
