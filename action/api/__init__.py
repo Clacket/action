96b376a0457fc084dfa04e960b99ff41030b9b94
Mariam Maarouf <mrf.mariam@gmail.com>
2018-01-27 03:13:36 +0200

    Show showings on movie page.

--- action/api/__init__.py
+++ action/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from sqlalchemy import and_
 from shapely.geometry import Point
 
-from action.models import Showing
+from action.models import Showing, MovieShowing
 # from action.utils import (
 #     authenticate, anonymous_only)
 
@@ -16,6 +16,21 @@ def nearby_cinemas():
     radius_meters = float(request.args.get('radius')) * 1000
     cinemas = Showing.query.filter(
         and_(
+            Showing.type == 'cinema',
+            Showing.geometry.ST_Distance(location) <= radius_meters
+        )).all()
+    cinemas_list = [x.serialize for x in cinemas]
+    return jsonify(cinemas=cinemas_list)
+
+
+@api.route('/movie/<int:movie_id>/showings/nearby')
+def nearby_showings(movie_id):
+    lat, lng = float(request.args.get('lat')), float(request.args.get('lng'))
+    location = 'SRID=4326;{0}'.format(Point(lng, lat).wkt)
+    radius_meters = float(request.args.get('radius')) * 1000
+    cinemas = Showing.query.join(MovieShowing).filter(
+        and_(
+            MovieShowing.movie_id == movie_id,
             Showing.type == 'cinema',
             Showing.geometry.ST_Distance(location) <= radius_meters
         )).all()
