36e6da892e3bc659844f14eec223919e246aa211
Mariam Maarouf <mrf.mariam@gmail.com>
2018-05-09 13:58:01 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,6 @@
-import os
-
-from dotenv import load_dotenv
-
 import pytest
 
 import action
-
-
-@pytest.fixture(scope='session', autouse=True)
-def dotenv():
-    """Load dotenv file."""
-    load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
-
 
 @pytest.fixture
 def app():
